cff7cf3f7ec7ebe912ae456ea938f5efab1adb88
pdaambrosio <pda.ambrosio@gmail.com>
2022-11-02 14:09:08 -0300

    feat: doc

--- binaryAddition.js
+++ binaryAddition.js
@@ -1,5 +1,15 @@
 function addBinary(a, b) {
-  /* A function that takes two arguments, a and b. */
+  /**
+   * A function that takes two arguments, a and b.
+   * Both arguments are integers.
+   * The function returns the sum of a and b in binary form.
+   * @param {number} a
+   * @param {number} b
+   * @return {string}
+   * @example
+   * addBinary(1, 2) // returns '11'
+   * addBinary(5, 9) // returns '1110'
+   */
   const sumNum = a + b;
   return sumNum.toString(2);
 }
