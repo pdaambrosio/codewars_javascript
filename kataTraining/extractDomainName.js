d79a118db9907759eb7170a3b407bfe4a00f782b
pdaambrosio <pda.ambrosio@gmail.com>
2022-11-24 08:02:17 -0300

    feat: extract domain name

---
+++ kataTraining/extractDomainName.js
@@ -0,0 +1,11 @@
+const domainName = (url) =>
+  /**
+   * It returns the domain name from a URL.
+   * @param url - The URL to extract the domain name from.
+   */
+  url.replace(/(https?:\/\/)?(www\.)?/, "").split(".")[0];
+
+console.log(domainName("http://google.com")); // "google"
+console.log(domainName("http://google.co.jp")); // "google"
+console.log(domainName("www.xakep.ru")); // "xakep"
+console.log(domainName("https://youtube.com")); // "youtube"
