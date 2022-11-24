d6e7953c857df2bbdf0b6d23002bab6646e2580a
pdaambrosio <pda.ambrosio@gmail.com>
2022-11-24 08:11:42 -0300

    fix:  protocol url test

--- kataTraining/extractDomainName.js
+++ kataTraining/extractDomainName.js
@@ -5,7 +5,7 @@ const domainName = (url) =>
    */
   url.replace(/(https?:\/\/)?(www\.)?/, "").split(".")[0];
 
-console.log(domainName("http://google.com")); // "google"
-console.log(domainName("http://google.co.jp")); // "google"
+console.log(domainName("https://google.com")); // "google"
+console.log(domainName("https://google.co.jp")); // "google"
 console.log(domainName("www.xakep.ru")); // "xakep"
 console.log(domainName("https://youtube.com")); // "youtube"
