d8c37b4c10271a9a1e5ec3332c7c5e3902267877
pdaambrosio <pda.ambrosio@gmail.com>
2023-01-11 10:00:54 -0300

    feat: vectorClass final

--- kataTraining/vectorClass.js
+++ kataTraining/vectorClass.js
@@ -1,14 +1,31 @@
+/* A Vector is an array of numbers that can be added, subtracted, dotted, and normed. */
 class Vector {
   constructor(arr) {
+    /**
+     * The constructor function takes an array as an argument and assigns it to the property arr.
+     * @param arr - The array to be sorted.
+     */
     this.arr = arr;
+  }
 
-    this.toString = function () {
-      return "(" + this.arr.join(",") + ")";
-    };
+  toString() {
+    return "(" + this.arr.join(",") + ")";
+  }
 
-    this.equals = function (calledEquals) {
-      return this.arr.length === calledEquals.length;
-    };
+  equals(calledEquals) {
+    calledEquals = calledEquals.arr;
+    let result = true;
+
+    if (calledEquals.length !== this.arr.length) {
+      return false;
+    }
+
+    for (let i = 0; i < calledEquals.length; i++) {
+      if (calledEquals[i] !== this.arr[i]) {
+        result = false;
+      }
+    }
+    return result;
   }
 
   add(calledAdd) {
@@ -22,7 +39,7 @@ class Vector {
     for (let i = 0; i < calledAdd.length; i++) {
       result[i] = this.arr[i] + calledAdd[i];
     }
-    return new Vector(result).arr;
+    return new Vector(result);
   }
 
   subtract(calledSubtract) {
@@ -36,12 +53,16 @@ class Vector {
     for (let i = 0; i < calledSubtract.length; i++) {
       result[i] = this.arr[i] - calledSubtract[i];
     }
-    return new Vector(result).arr;
+    return new Vector(result);
   }
 
   dot(calledDot) {
     calledDot = calledDot.arr;
     let result = 0;
+
+    if (calledDot.length !== this.arr.length) {
+      throw new Error("Error");
+    }
 
     for (let i = 0; i < calledDot.length; i++) {
       result += this.arr[i] * calledDot[i];
@@ -58,18 +79,13 @@ class Vector {
   }
 }
 
-// var a = new Vector([1, 2, 3]);
-// var b = new Vector([3, 4, 5]);
-// var c = new Vector([5, 6, 7, 8]);
+var a = new Vector([1, 2, 3]);
+var b = new Vector([3, 4, 5]);
+var c = new Vector([5, 6, 7, 8]);
 
-// console.log(a.add(b));
-// console.log(a.subtract(b));
-// console.log(a.dot(b));
-// console.log(a.norm());
-// console.log(a.equals(new Vector([4, 6])));
-// console.log(a.add(c));
-
-var a = new Vector([1, 2]);
-var b = new Vector([3, 4]);
-
-console.log(a.add(b).equals(new Vector([4, 6])));
+console.log(a.add(b)); // should return Vector([4,6,8])
+console.log(a.subtract(b)); // should return Vector([-2,-2,-2])
+console.log(a.dot(b)); // should return 1*3+2*4+3*5 = 26
+console.log(a.norm()); // should return sqrt(1^2+2^2+3^2)=sqrt(14)
+console.log(a.equals(new Vector([4, 6]))); // should return false
+console.log(a.add(c)); // throws an error
