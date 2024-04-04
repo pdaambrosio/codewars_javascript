851ff289a1e6f9854d5b2b6686d72445f49ea29b
pdaambrosio <pda.ambrosio@gmail.com>
2024-04-04 10:22:02 -0300

    feat: oppositeNumber

---
+++ oppositeNumber.js
@@ -0,0 +1,16 @@
+function oppositeNumber(number) {
+    /**
+     * A function that takes one argument, a number.
+     * The function returns the opposite of the number.
+     * @param {number} number
+     * @return {number}
+     * @example
+     * oppositeNumber(1) // returns -1
+     * oppositeNumber(-5) // returns 5
+     */
+    return -number;
+}
+
+console.log(oppositeNumber(1)); // -1
+console.log(oppositeNumber(-5)); // 5
+console.log(oppositeNumber(0)); // 0
